942445db34eb4872a8d21cecad69bd65b2d3078d
sssukh <sssukh50@gmail.com>
2023-03-05 00:00:20 +0900

    69.Sqrt(x)

---
+++ leetcode_69/leetcode_69/source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Solution 
+{
+public:
+    int mySqrt(int x) 
+    {
+        unsigned int current=x,left = 0, right = x;
+
+        while (right-left>1)
+        {
+            current = (left + right) / 2;
+            int temp = current * current;
+            
+            if (temp<0||temp < current || temp > x)
+            {
+                right = current;
+            }
+            else if (temp < x)
+            {
+                left = current;
+            }
+            else if (temp == x)
+                break;
+        }
+
+        if (current * current > x)
+            current -= 1;
+
+        return current;
+    }
+};
+
+
+int main()
+{
+    Solution sol;
+    sol.mySqrt(2147395599);
+}
