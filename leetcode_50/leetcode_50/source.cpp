a524bb4a6111caf3971cca44e9e7301789e7653e
sssukh <sssukh50@gmail.com>
2023-03-24 21:38:42 +0900

    50.Pow(x,n)

---
+++ leetcode_50/leetcode_50/source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    double myPow(double x, int n) 
+    {
+        if (n == 0)
+            return 1;
+        int absoluteN = abs(n);
+       /* if (n < 0)
+            absoluteN = -n;*/
+
+        double tmp = myPow(x, absoluteN / 2);
+        double answer;
+        if (absoluteN % 2)
+        {   
+            answer = tmp * tmp * x;
+        }
+        else
+        {
+            answer = tmp * tmp;
+        }
+
+        if (n < 0)
+            return 1 / answer;
+        else
+            return answer;
+
+    }
+};
