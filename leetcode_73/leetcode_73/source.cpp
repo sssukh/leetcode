986c6c041f74776b08a2ff2226c8d353ffe3938a
sssukh <sssukh50@gmail.com>
2023-05-05 20:41:03 +0900

    73.Set Matrix Zeroes

--- leetcode_73/leetcode_73/source.cpp
+++ leetcode_73/leetcode_73/source.cpp
@@ -7,26 +7,33 @@ class Solution
 public:
     void setZeroes(vector<vector<int>>& matrix) 
     {
-        unordered_set<int> zeroRows;
+        //unordered_set<int> zeroRows;
         unordered_set<int> zeroCols;
+        vector<int> zeros(matrix[0].size(), 0);
+       
         for (int i = 0; i < matrix.size(); ++i)
         {
+            bool isZeroRow = false;
             for (int j = 0; j < matrix[0].size(); ++j)
             {
                 if (matrix[i][j] == 0)
                 {
-                    zeroRows.insert(i);
+                    isZeroRow = true;
+                    //zeroRows.insert(i);
                     zeroCols.insert(j);
                 }
             }
+            if (isZeroRow)
+                matrix[i] = zeros;
         }
-        for (auto iter : zeroRows)
-        {
-            for (int cols = 0; cols < matrix[0].size(); ++cols)
-            {
-                matrix[iter][cols] = 0;
-            }
-        }
+        //for (auto iter : zeroRows)
+        //{
+        //    /*for (int cols = 0; cols < matrix[0].size(); ++cols)
+        //    {
+        //        matrix[iter][cols] = 0;
+        //    }*/
+        //    matrix[iter] = zeros;
+        //}
         for (auto iter : zeroCols)
         {
             for (int rows = 0; rows < matrix.size(); ++rows)
