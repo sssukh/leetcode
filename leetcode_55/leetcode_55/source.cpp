654ce9f45184c02bd731016f11b539c54e7bf881
sssukh <sssukh50@gmail.com>
2023-05-07 22:35:22 +0900

    55.Jump Game

--- leetcode_55/leetcode_55/source.cpp
+++ leetcode_55/leetcode_55/source.cpp
@@ -6,13 +6,42 @@ class Solution
 public:
     bool canJump(vector<int>& nums)
     {
-        int startIdx = nums.size() - 1;
+       /* int startIdx = nums.size() - 1;
         for (int idx = nums.size() - 1; idx >= 0; --idx)
         {
             if (nums[idx] >= startIdx-idx)
                 startIdx = idx;
         }
         
-        return startIdx == 0;
+        return startIdx == 0;*/
+        int i = 0;
+        while (i<nums.size()-1)
+        {
+            if (nums[i] == 0)
+            {
+                int zeroIdx = i;
+                while (i > 0 && zeroIdx - i >= nums[i])
+                {
+                    --i;
+                }
+                if (i == 0)
+                    return false;
+                else
+                    i += nums[i];
+            }
+            else
+            {
+                i += nums[i];
+            }
+       
+        }
+        return true;
     }
 };
+
+int main()
+{
+    Solution sol;
+    vector<int> test = { 2,2,0,2,0,2,0,0,2,0 };
+    sol.canJump(test);
+}
