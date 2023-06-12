cb8e376fe4c297204220949e091c1b7e4ec0260a
sssukh <sssukh50@gmail.com>
2023-06-12 21:41:47 +0900

    238.Product of Array Except Self

--- leetcode_238/source.cpp
+++ leetcode_238/source.cpp
@@ -7,29 +7,18 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
         int size = nums.size();
-        vector<int> numsleft(size);
         vector<int> ans(size);
+        ans[0] = 1;
+        for (int i =1;i<size;++i)
+        {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
         int pro = 1;
-        for (int i = size - 1; i >= 0; --i)
+        for (int i = size - 2; i >= 0; --i)
         {
-            pro *= nums[i];
-            numsleft[i] = pro;
+            pro *= nums[i + 1];
+            ans[i] = ans[i] * pro;
         }
-        ans[0] = numsleft[1];
-        int left = 1;
-        for (int i = 1; i < size; ++i)
-        {
-            left *= nums[i-1];
-            
-            int right;
-            if (i + 1 >= size)
-                right = 1;
-            else
-                right = numsleft[i + 1];
-
-            ans[i] = left * right;
-        }
-
         return ans;
     }
 };
